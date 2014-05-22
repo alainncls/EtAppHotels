4767e71e7e40f5f55671a5304d32720a6123d19d
zorator <vivien.auguy@epf-projets.fr>
2014-05-22 15:40:36 +0200

    pouf

--- app/scripts/controllers/hotel.js
+++ app/scripts/controllers/hotel.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var urlJSON = 'http://public.opendatasoft.com/api/records/1.0/search?dataset=hotels-classes-en-france&q=recordid:';
+var urlJSON = 'http://public.opendatasoft.com/api/records/1.0/';
+urlJSON += 'search?dataset=hotels-classes-en-france';
+urlJSON += '&q=recordid:';
 
 angular.module('projetAngularJsApp')
 .controller('HotelCtrl', function ($scope, $http, $routeParams) {
 	$scope.hotel = [];
 
-	$http.get(urlJSON+'hotels_classescsvzip/'+$routeParams.id).success( function (data) {
-		$scope.hotel=data.records;
+	$http.get(urlJSON+$routeParams.id).success( function (data) {
+		$scope.hotel=data.records[0];
 	});
 });
