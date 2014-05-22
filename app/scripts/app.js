ce76012ad1e6241c4fcf2a95bbb7d9539ec9c7c1
alainncls <alain.nicolas@epfedu.fr>
2014-05-22 15:02:48 +0200

    osef aussi

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,6 +18,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'MainCtrl'
       })
+      .when('/hotel/:id', {
+        templateUrl: 'views/hotel.html',
+        controller: 'MainCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
